2d50db97941ac63fb6dc82d8d31379c08487fc6f
shengwei.li <lucasleesw9@gmail.com>
2020-05-13 17:20:59 -0400

    colab vgg

--- interface.py
+++ interface.py
@@ -4,6 +4,7 @@ from PCANet import *
 from data_loader import *
 import sys
 from sklearn.metrics import accuracy_score
+from sklearn import svm
 
 # train_images, train_labels, test_images, test_labels = load_mnist('data/MNIST')
 # test_train = (train_images[:10, :, :], train_labels[:10])
@@ -19,6 +20,9 @@ test_train = (cifar_train[:10, :, :, :], cifar_train_labels[:10])
 print(test_train[0].shape, test_train[1].shape)
 
 net = PCANet(k1=5, k2=5, L1=40, L2=8, block_size=8, overlapping_radio=0.5, spp_parm=(4, 2, 1), dim_reduction=1280)
+
+net.classifier = svm.LinearSVC(C=10)
+
 net.fit(*test_train)
 test_predict = cifar_test[:10, :, :]
 prediction = net.predict(test_predict)

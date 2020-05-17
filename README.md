c2e0ffb762e055707f9e9d7cd58d59d86e98a8a5
shengwei.li <lucasleesw9@gmail.com>
2020-05-17 00:54:13 -0400

    colab vgg

--- README.md
+++ README.md
@@ -3,3 +3,41 @@ SBU CSE512 Project
 
 A simple implementation of PCANet: A Simple Deep Learning Baseline for Image Classification?
 https://arxiv.org/abs/1404.3606
+
+Usage of PCANet:
+
+- Example: See interface.py and PCANet-MNIST.ipynb
+
+- General use process:
+
+  1. Load data into numpy ndarray(check data_loader.py and data_loader2.py as example)
+  2. Initialize PCANet class, e.g.:
+
+  `net = PCANet(k1=7, k2=7, L1=8, L2=8, block_size=7)`
+
+  Other paramaters:
+  ```python
+  overlapping_radio=0 
+  # overlapping radio, 1-0, optional, default 0
+  linear_classifier='svm' 
+  # linear_classifier, a linear_classifer or 'svm', 'svm' means sklearn.svm.SVC(), optional, default 'svm'
+  spp_parm=None
+  # parameters for spp, needs to be a list, eg[4,2,1], optional, default none
+  dim_reduction=None
+  # dim reduction after spp, the number of dimension reduce to, optional, default none
+  ```
+
+  3.  Train PCANet with train data, e.g.:
+
+  `net.fit(train_data)`
+
+  4.  Predict test data's label with trained PCANet, e.g.:
+
+  `prediction = net.predict(test_data)`
+
+
+
+Usage of dataloader:
+
+#todo
+
--- interface.py
+++ interface.py
@@ -6,24 +6,24 @@ import sys
 from sklearn.metrics import accuracy_score
 from sklearn import svm
 
-# train_images, train_labels, test_images, test_labels = load_mnist('data/MNIST')
-# test_train = (train_images[:10, :, :], train_labels[:10])
+train_images, train_labels, test_images, test_labels = load_mnist('data/MNIST')
+test_train = (train_images[:10, :, :], train_labels[:10])
 
-# net = PCANet(k1=7, k2=7, L1=8, L2=8, block_size=7, overlapping_radio=0)
+net = PCANet(k1=7, k2=7, L1=8, L2=8, block_size=7, overlapping_radio=0)
 
-# test_predict = test_images[:10, :, :]
-# prediction = net.predict(test_predict)
-# print(accuracy_score(test_labels[:10], prediction))
-
-cifar_train, cifar_train_labels, cifar_test, cifat_test_label = load_CIFAR10('data/cifar-10-batches-py')
-test_train = (cifar_train[:10, :, :, :], cifar_train_labels[:10])
-print(test_train[0].shape, test_train[1].shape)
-
-net = PCANet(k1=5, k2=5, L1=40, L2=8, block_size=8, overlapping_radio=0.5, spp_parm=(4, 2, 1), dim_reduction=1280)
-
-net.classifier = svm.LinearSVC(C=10)
-
-net.fit(*test_train)
-test_predict = cifar_test[:10, :, :]
+test_predict = test_images[:10, :, :]
 prediction = net.predict(test_predict)
-print('acc:', accuracy_score(cifat_test_label[:10], prediction))
+print(accuracy_score(test_labels[:10], prediction))
+#
+# cifar_train, cifar_train_labels, cifar_test, cifat_test_label = load_CIFAR10('data/cifar-10-batches-py')
+# test_train = (cifar_train[:10, :, :, :], cifar_train_labels[:10])
+# print(test_train[0].shape, test_train[1].shape)
+#
+# net = PCANet(k1=5, k2=5, L1=40, L2=8, block_size=8, overlapping_radio=0.5, spp_parm=(4, 2, 1), dim_reduction=1280)
+#
+# net.classifier = svm.LinearSVC(C=10)
+#
+# net.fit(*test_train)
+# test_predict = cifar_test[:10, :, :]
+# prediction = net.predict(test_predict)
+# print('acc:', accuracy_score(cifat_test_label[:10], prediction))
